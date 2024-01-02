7b619002b92621c502ac8b4f9918707ccc826da0
motivating12 <155353359+motivating12@users.noreply.github.com>
2024-01-02 15:45:42 +0800

    Add files via upload

--- motivating_ex.c
+++ motivating_ex.c
@@ -1,43 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-void dummy();
 
 char sensitive_data[20];
 
-char *copy(char *dest, const char* src){
-	return strcpy(dest, src);
-}
-
-void pass(){
-}
-void fail(){
-}
-
-
-void op(char *(str_op)(char *, const char *), const char *data){
-	(*str_op)(sensitive_data, data); 
+void sink_mtd(){
     char dest[50];
-	memcpy(dest,sizeof(sensitive_data),sensitive_data);
+	memcpy(dest,sensitive_data,sizeof(sensitive_data));
 }
 
-int auth(const char *buf){
-	if( !strcmp(buf, "passwd") ){
+int auth(){
 		char data[100];
 		fgets(data,sizeof(data),stdin);
-		op(copy, data); //SLIVER_SOURCE
-		return 1;
-	}
-	return 0;
+		strcpy(sensitive_data,data);
+		sink_mtd();	
 }
-void dummy(){
-	printf("dummy\n");
-}
-void main(int argc, char **argv){
-	int auth_ret=auth(argv[1]);
-	if (auth_ret){
-		dummy();
-		pass();
-	} else {
-		fail();
-	}
+
+void main(){
+	auth();
+
+	
 }
