c00a156d578bcdebed2db7ae84774173fd8d42a0
motivating12 <155353359+motivating12@users.noreply.github.com>
2024-01-02 15:55:04 +0800

    Add files via upload

--- motivating_ex.c
+++ motivating_ex.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-char sensitive_data[20];
+char sensitive_data[50];
 
 void sink_mtd(){
-    char dest[50];
+    char dest[20];
 	memcpy(dest,sensitive_data,sizeof(sensitive_data));
 }
 
 int auth(){
-		char data[100];
+		char data[40];
 		fgets(data,sizeof(data),stdin);
 		strcpy(sensitive_data,data);
 		sink_mtd();	
